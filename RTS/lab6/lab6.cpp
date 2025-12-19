71600f5c7bfc7aba6c952674ed1648f2329cd428
Vladimir <Vladopadus@mail.ru>
2025-12-19 13:06:41 +0200

    пункт 1 для lab6

--- RTS/lab6/lab6.cpp
+++ RTS/lab6/lab6.cpp
@@ -4,6 +4,83 @@
 #include <thread>
 using namespace std;
 
+/*
+struct CoroutineResult {
+    struct promise_type;
+    using handle_type = std::coroutine_handle<promise_type>;
+
+    struct promise_type {
+        int current_value;
+
+        CoroutineResult get_return_object() {
+            return CoroutineResult{ handle_type::from_promise(*this) };
+        }
+
+        std::suspend_always initial_suspend() { return {}; }
+        std::suspend_always final_suspend() noexcept { return {}; }
+        void return_void() {}
+        void unhandled_exception() {}
+
+        std::suspend_always yield_value(int value) {
+            current_value = value;
+            return {};
+        }
+    };
+
+    handle_type coro_handle;
+
+    //конструктор
+    explicit CoroutineResult(handle_type h) : coro_handle(h) {}
+
+    //деструктор
+    ~CoroutineResult() {
+        if (coro_handle) {
+            coro_handle.destroy();
+        }
+    }
+
+    int value() const {
+        return coro_handle.promise().current_value;
+    }
+
+    bool resume() {
+        if (!coro_handle.done()) {
+            coro_handle.resume();
+        }
+        return !coro_handle.done();
+    }
+
+    bool done() const {
+        return coro_handle.done();
+    }
+};
+
+CoroutineResult generate_numbers() {
+    int my_number = 167;
+    std::cout << "Генерация чисел из числа " << my_number << std::endl;
+    co_yield my_number;           //1 число
+    co_yield my_number * 2;       //2
+    co_yield my_number + 10;      //3
+    co_yield my_number - 5;       //4
+    co_yield my_number * my_number; //5
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    CoroutineResult numbers = generate_numbers();
+    int counter = 1;
+    while (numbers.resume()) {
+        std::cout << "Число " << counter << ": " << numbers.value() << std::endl;
+        counter++;
+    }
+    std::cout << std::endl;
+    std::cout << "Корутина завершила выполнение." << std::endl;
+
+    return 0;
+}
+*/
+
 struct promise;
 
 struct promise_type
@@ -98,4 +175,5 @@ int main()
     std::cout << "\ndone" << std::endl;
 
     return 0;
+
 }
